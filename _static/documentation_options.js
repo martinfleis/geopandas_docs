1d27f87b5e47fa2cac63b776e4d90a84c9b2af88
Martin Fleischmann <martin@martinfleischmann.net>
2020-08-07 22:46:13 +0100

    update

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.0+28.g70986c0.dirty',
+    VERSION: '0.8.0+31.g33f6d30',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
